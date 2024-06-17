89d434a16ec93ccd57882fce3e7bba5afd83ab10
Aryan khera <105739020+AryAnKhera23@users.noreply.github.com>
2024-06-17 19:42:10 +0530

    UI service Service Locator

--- Assets/Scripts/Main/GameService.cs
+++ Assets/Scripts/Main/GameService.cs
@@ -1,4 +1,6 @@
 using ServiceLocator.Player;
+using ServiceLocator.Sound;
+using ServiceLocator.UI;
 using ServiceLocator.Utilities;
 using System.Collections;
 using System.Collections.Generic;
@@ -7,12 +9,21 @@ using UnityEngine;
 public class GameService : GenericMonoSingleton<GameService>
 {   
     public PlayerService playerService { get; private set; }
+    public SoundService soundService { get; private set; }
 
-    [SerializeField] public PlayerScriptableObject playerScriptableObject;
+    [SerializeField] private UIService uiService;
+    public UIService UIService => uiService;
+
+    [SerializeField] private PlayerScriptableObject playerScriptableObject;
+    [SerializeField] private SoundScriptableObject soundScriptableObject;
+
+    [SerializeField] private AudioSource audioEffects;
+    [SerializeField] private AudioSource backgroundMusic;
 
     private void Start()
     {
         playerService = new PlayerService(playerScriptableObject);
+        soundService = new SoundService(soundScriptableObject, audioEffects, backgroundMusic);
     }
 
     private void Update()
